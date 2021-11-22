4a099c7fb877655955024ea0b434f6701c8e03e1
CQYaDoKaRi <cqyadokari@gmail.com>
2021-11-22 17:53:39 +0900

    [submitjob] config.sh に submitjob の項目を追加（関連するプログラムのコメントを追記）

--- config.sh
+++ config.sh
@@ -4,41 +4,91 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # ---------------------------------
 # 環境変数
 # ---------------------------------
+# 動作確認用のアプリケーションバージョン
+# ・関連するプログラム
+#   - run.sh
+VERSION="1.0"
+
 # リージョン
+# ・関連するプログラム
+#   - ecr.sh
 AWS_REGION="ap-northeast-1"
 
-# S3バケット名
-AWS_BATCH_BACKET=""
-
 # ECR - アカウントID
+# ・関連するプログラム
+#   - ecr.sh
 AWS_ECR_AID=""
+# ・関連するプログラム
+#   - ecr.sh
 # ECR - レポジトリ名
 AWS_ECR_REP=""
+
+# S3バケット名
+# ・関連するプログラム
+#   - run.sh
+AWS_BATCH_BACKET=""
 
 # ---------------------------------
 # config.sh 内のプログラムの設定
 # ---------------------------------
 # データ - 拡張子
+# ・関連するプログラム
+#   - run.sh
 DATA_EXT=".dat"
 
 # データ - ローカルフォルダー（S3 アップロードデータ=インプットデータ）
+# ・関連するプログラム
+#   - run.sh
 DATA_DIR_SRC="${DIR}/data.src"
 # データ - ローカルフォルダー
+# ・関連するプログラム
+#   - run.sh
 DATA_DIR="${DIR}/data"
 # データ - ローカルファイル
+# ・関連するプログラム
+#   - run.sh
 DATA_FILE="data.tar.gz"
 # データ - S3バケット：フォルダー名
+# ・関連するプログラム
+#   - run.sh
 DATA_BACKET_PATH="/data/"
 
 # 結果 - ローカルフォルダー
+# ・関連するプログラム
+#   - run.sh
 OUT_DIR="${DIR}/out"
 # 結果 - ローカルファイル（圧縮）
+# ・関連するプログラム
+#   - run.sh
 OUT_FILE="out.tar.gz"
 # 結果 - S3バケット：フォルダー名
+# ・関連するプログラム
+#   - run.sh
 OUT_BACKET_PATH="/out/"
 
 # コンテナ - イメージタグ名(docker-compose.yml のimageに合わせる)
+# ・関連するプログラム
+#   - ecr.sh
 DOCKER_IMAGE_TAG="aws_batch:dev"
+
+# コンテナ - コンテナ名(docker-compose.yml のcontainer_nameに合わせる)
+# ・関連するプログラム
+#   - docker_exec.sh
+#   - docker_exec_run.sh
+DOCKER_CONTAINER_NAME="aws_batch_dev"
+
+# ジョブ定義
+# ・関連するプログラム
+#   - job_common.sh
+JOB_DEFINITION=""
+# ジョブ定義：バージョン
+# ・関連するプログラム
+#   - job_common.sh
+JOB_DEFINITION_VERSION=""
+# ジョブキュー
+# ・関連するプログラム
+#   - job_common.sh
+JOB_QUEUE=""
 
 # ---------------------------------
 # config.sh を上書きする外部ファイル
@@ -81,6 +131,10 @@ fi
 # - バケット
 # ---------------------------------
 # データ - S3バケット
+# ・関連するプログラム
+#   - run.sh
 DATA_BACKET="${AWS_BATCH_BACKET}${DATA_BACKET_PATH}"
 # 結果 - S3バケット
+# ・関連するプログラム
+#   - run.sh
 OUT_BACKET="${AWS_BATCH_BACKET}${OUT_BACKET_PATH}"
