fcc548139f59cf77cc1963692f2541bdb27f4adf
CQYaDoKaRi <cqyadokari@gmail.com>
2021-11-22 17:55:54 +0900

    [submitjob] docker_exec*.sh のコンテナ名を config.sh を参照するように変更

--- docker_exec_run.sh
+++ docker_exec_run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker exec -it aws_batch /usr/local/aws.batch/run.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${DIR}/config.sh
+
+if [ -e ${DIR}/config.env.sh ]; then
+    echo "[config.sh] copy ${DOCKER_CONTAINER_NAME}:/usr/local/aws.batch/config.env.sh"
+    docker cp ${DIR}/config.env.sh ${DOCKER_CONTAINER_NAME}:/usr/local/aws.batch/config.env.sh
+fi
+docker exec -it ${DOCKER_CONTAINER_NAME} /usr/local/aws.batch/run.sh
